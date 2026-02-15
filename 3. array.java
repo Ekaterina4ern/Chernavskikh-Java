f2c6276c7560a262668f6fe22ea708d08ac923b6
Ekaterina <kcdoc1993@gmail.com>
2026-02-16 02:26:42 +0500

    Add files via upload

---
+++ 3. array.java
@@ -0,0 +1,14 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        System.out.print("Чётные числа: ");
+        
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println(); 
+    }
+}
